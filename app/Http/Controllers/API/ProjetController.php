80ad0b0f9dad680779068711deaa15a803862504
Nicolas BRAILLON <nicolas.braillonpro@gmail.com>
2023-03-20 11:29:00 +0100

    Ajout routes API + création méthodes controller Projet

--- app/Http/Controllers/API/ProjetController.php
+++ app/Http/Controllers/API/ProjetController.php
@@ -5,46 +5,82 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Projet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProjetController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        $projets = DB::table('projets')
+            ->get()
+            ->toArray();
+
+        return response()->json([
+            'status' => 'Success', 'data' => $projets,
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nom' => 'required|max:250',
+            'description' => 'required',
+            'date_debut' => 'required|date|date_format:d/m/Y',
+            'date_fin' => 'required|date|date_format:d/m/Y',
+            'avancement' => 'required|integer',
+        ]);
+
+        $projet = Projet::create([
+            'nom' => $request->nom,
+            'description' => $request->description,
+            'date_debut' => $request->date_debut,
+            'date_fin' => $request->date_fin,
+            'avancement' => $request->avancement,
+        ]);
+
+        return response()->json([
+            'status' => 'Success', 
+            'data' => $projet,
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Projet $projet)
     {
-        //
+        return response()->json($projet);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Projet $projet)
     {
-        //
+        $request->validate([
+            'nom' => 'required|max:250',
+            'description' => 'required',
+            'date_debut' => 'required|date|date_format:d/m/Y',
+            'date_fin' => 'required|date|date_format:d/m/Y',
+            'avancement' => 'required|integer',
+        ]);
+
+        $projet->update([
+            'nom' => $request->nom,
+            'description' => $request->description,
+            'date_debut' => $request->date_debut,
+            'date_fin' => $request->date_fin,
+            'avancement' => $request->avancement,
+        ]);
+
+        return response()->json([
+            'status' => 'Update Successfully', 
+            'data' => $projet,
+        ]);
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Projet $projet)
     {
-        //
+        $projet->delete();
+
+        return response()->json([
+            'status' => 'Delete Successfully'
+        ]);
     }
 }
--- app/Http/Controllers/API/TacheController.php
+++ app/Http/Controllers/API/TacheController.php
@@ -5,44 +5,36 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Tache;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TacheController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $taches = DB::table('taches')
+            ->get()
+            ->toArray();
+
+        return response()->json([
+            'status' => 'Success', 'data' => $taches,
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Tache $tache)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Tache $tache)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Tache $tache)
     {
         //

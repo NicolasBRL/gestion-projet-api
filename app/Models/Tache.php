31e5bfd03cbf86774c6dc34d9862759534c451dd
Nicolas BRAILLON <nicolas.braillonpro@gmail.com>
2023-03-20 11:17:08 +0100

    Création controller + model

---
+++ app/Models/Tache.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tache extends Model
+{
+    use HasFactory;
+    protected $fillable = ['id', 'nom', 'description', 'date_debut', 'date_fin', 'avancement'];
+}
